cb1031ff1919c66d218fdb5a607e33a6d36232b8
saufiroja <saufiroja08@gmail.com>
2025-05-24 15:27:10 +0800

    chore: remove user id

--- migrations/010_chat_messages.sql
+++ migrations/010_chat_messages.sql
@@ -5,12 +5,10 @@ CREATE TYPE chat_message_sender AS ENUM ('user', 'ai');
 CREATE TABLE chat_messages (
     chat_message_id VARCHAR(250) PRIMARY KEY,
     chat_session_id VARCHAR(250) NOT NULL,
-    user_id VARCHAR(250) NOT NULL,
     sender chat_message_sender NOT NULL,
     message TEXT NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP,
     deleted_at TIMESTAMP,
-    CONSTRAINT fk_chat_messages_chat_session FOREIGN KEY (chat_session_id) REFERENCES chat_sessions(chat_session_id),
-    CONSTRAINT fk_chat_messages_user FOREIGN KEY (user_id) REFERENCES users(user_id)
+    CONSTRAINT fk_chat_messages_chat_session FOREIGN KEY (chat_session_id) REFERENCES chat_sessions(chat_session_id)
 );
